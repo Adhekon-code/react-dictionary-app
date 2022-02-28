55ab5445b4fd87ec88a52ce20cd320ed63c715a9
Adhekon-code <92987196+Adhekon-code@users.noreply.github.com>
2022-02-28 16:09:31 +0300

    Added axios, and worked on the API

--- src/Search.js
+++ src/Search.js
@@ -1,24 +1,33 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import './Search.css';
 
 export default function Search(){
-    let [keyword, setKeyword]=useState("")
+let [keyword, setKeyword]=useState("")
+
+function handleResponse(response){
+console.log (response)
+}
+
 function Search(event)
 {
 event.preventDefault();
-alert (`Searching for ${keyword} definition...`);
-    }
+
+
+ let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+ console.log(apiUrl); 
+axios.get(apiUrl).then(handleResponse)
+  }
 function handleKeywordchange(event){
 console.log(event.target.value);
 setKeyword(event.target.value);
-}   
-return <div className="Search">
+} 
 
+
+return <div className="Search">
 <form onSubmit={Search}>
  <input type="search" onChange={handleKeywordchange} autoFocus={true}/>  
 </form>
-
-
 
 </div>;
 }
