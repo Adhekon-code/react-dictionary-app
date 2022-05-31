621e828b3fb74882c08ca48fcb782966633b20a1
Adhekon-code <92987196+Adhekon-code@users.noreply.github.com>
2022-05-31 19:25:28 +0300

    Updated photos

--- src/Photos.js
+++ src/Photos.js
@@ -8,7 +8,7 @@ export default function Photos(props) {
             <div className="row">
             {props.photos.map(function (photo, index)
             {
-                return <img src={photo.src.tiny} target="_blank" key={index} />;
+                return <img src={photo.src.tiny} alt="_blank" rel="noreferrer" key={index} />;
         })}
         </div>
         </section>
